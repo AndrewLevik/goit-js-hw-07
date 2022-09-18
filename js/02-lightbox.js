c94fcef538a795d1dc804134d2e8b2ef8befeba7
Andrew <andrew.levik@gmail.com>
2022-09-19 00:29:13 +0200

    end HW

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((element) => {
+  const galleryItem = document.createElement("a");
+  galleryItem.className = "gallery__item";
+  galleryItem.href = element.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("data-source", element.original);
+  galleryImage.alt = element.description;
+
+  galleryItem.append(galleryImage);
+
+  items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionPosition: "outside",
+  captionDelay: 250,
+  captionsData: "alt",
+});
